1ce15a08de76e979a895ae334c26720fbad4ae26
Kai Si <kai.si@berkeley.edu>
2014-10-04 16:40:06 -0700

    try templating

--- server.py
+++ server.py
@@ -1,17 +1,15 @@
 import os
 from flask import Flask, render_template, request
-from urllib2 import urlopen
-from xml.dom import minidom
-from random import choice
+import findrecipe
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET"])
 def main_page():
-    return render_template('index.html')
+    return render_template('index.html', input="", recipename="Beerd Eggs")
 
 @app.route('/', methods=['POST'])
 def recipe_post():
-    text = request.form['input']
-    processed_text = text.upper()
-    return processed_text
+    inputrecipe = request.form['input']
+    name, ingredients, instructions = findRecipe(inputrecipe)
+    return render_template('index.html', input=inputrecipe, recipename=name)
