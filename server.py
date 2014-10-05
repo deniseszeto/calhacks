a06091b5ea6cfaad610f8be20a873db5d87b3cb8
Kai Si <kai.si@berkeley.edu>
2014-10-04 23:59:02 -0700

    some more updates

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 import os
 from flask import Flask, render_template, request
 from findrecipe import findRecipe
+from nearestStore import nearestStore
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
 def main_page():
     if request.method == "GET":
-        return render_template('index.html', input="", recipename="", ingred=set(), recipe="")
+        return render_template('index.html', input="", recipename="", ingred=set(), recipe="", address="")
     else:
         inputrecipe = request.form['input']
+        inputlat = request.form['lat']
+        inputlong = request.form['long']
         name, ingredients, ctype, instructions = findRecipe(inputrecipe)
+        address = nearestStore(ctype,inputlat,inputlong)
+        print(address)
         return render_template('index.html', input=inputrecipe, recipename=name,
-                               ingred=ingredients, recipe=instructions)
+                               ingred=ingredients, recipe=instructions, address=address)
 
 if __name__ == '__main__':
     app.debug = True
