f0ddee9f13b4b67dedbef40c6263b5a91f58c039
Kai Si <kai.si@berkeley.edu>
2014-10-04 18:18:13 -0700

    recipe and instructions showing up

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 import os
 from flask import Flask, render_template, request
-import findrecipe
+from findrecipe import findRecipe
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
 def main_page():
     if request.method == "GET":
-        return render_template('index.html', input="", recipename="Beer Eggs")
+        return render_template('index.html', input="", recipename="Beer Eggs", ingred=set(), recipe="")
     else:
         inputrecipe = request.form['input']
-        print(inputrecipe)
         name, ingredients, instructions = findRecipe(inputrecipe)
-        print(name)
-        return render_template('index.html', input=inputrecipe, recipename=name)
+        return render_template('index.html', input=inputrecipe, recipename=name,
+                               ingred=ingredients, recipe=instructions)
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run(port=5555)
